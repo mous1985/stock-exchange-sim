4c5cdc725cdfcf2b56aff71e938e82f4de0c2cac
mus_36 <mustapha.benazzouz.213@gmail.com>
2024-01-05 14:25:13 +0100

    package prod

--- main.go
+++ main.go
@@ -3,47 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
+
+	"stockexchange/production"
 )
 
 func main() {
-	// Chemin du fichier à lire
-	fileName := "./examples/" + os.Args[1]
-
-	// Lecture du contenu du fichier
-	content, err := os.ReadFile(fileName)
+	// Read the file
+	arg := os.Args
+	if len(arg) != 3 {
+		fmt.Println("Usage : go run . <File name> <waiting_time seconde>")
+		return
+	}
+	timer, err := strconv.ParseFloat(arg[2], 32)
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture du fichier :", err)
+		fmt.Println("Error while parsing `" + arg[2] + "`")
+		os.Exit(0)
+	}
+	data, err := os.ReadFile("examples/simple")
+	if err != nil {
+		fmt.Println("File reading error", err)
 		return
 	}
 
-	// Conversion du contenu du fichier en une chaîne de caractères
-	input := string(content)
-
-	// Séparation de la chaîne d'entrée par lignes
-	lines := strings.Split(input, "\n")
-
-	// Parcours des lignes pour extraire les informations pertinentes
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-
-		// Ignorer les lignes vides ou les commentaires
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		// Analyse des différentes parties de la ligne
-		if strings.HasPrefix(line, "optimize:") {
-			// Extraction des éléments à optimiser
-			parts := strings.Split(strings.TrimPrefix(line, "optimize:"), "|")
-			for _, part := range parts {
-				fmt.Println("Optimiser :", part)
-			}
-		} else if strings.Contains(line, ":") {
-			// Extraction des informations de stock et de processus
-			data := strings.Split(line, ":")
-			fmt.Println("Nom :", data[0])
-			fmt.Println("Valeur :", data[1])
-		}
-	}
+	production := production.Production{}
+	production.Timeout = true
+	fmt.Println(production.Timeout)
 }
---
+++ production/production.go
@@ -0,0 +1,11 @@
+package production
+
+type Production struct {
+	Timeout   bool
+	File_name string
+	// stocks               []Stock
+	// processes            []Processe
+	// to_optimize          Optimize
+	// Processe_in_Progress []Processe_Progress
+	Current_cycle int
+}
