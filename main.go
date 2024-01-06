e2d6cfa6c3dc78031e53edd0fb74edf33166e9e2
mus_36 <mustapha.benazzouz.213@gmail.com>
2024-01-06 13:41:43 +0100

    print content file

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"stockexchange/production"
 )
 
 func main() {
 	// Read the file
-	arg := os.Args
-	if len(arg) != 3 {
-		fmt.Println("Usage : go run . <File name> <waiting_time seconde>")
-		return
-	}
-	timer, err := strconv.ParseFloat(arg[2], 32)
-	if err != nil {
-		fmt.Println("Error while parsing `" + arg[2] + "`")
-		os.Exit(0)
-	}
+	// arg := os.Args
+	// if len(arg) != 3 {
+	// 	fmt.Println("Usage : go run . <File name> <waiting_time seconde>")
+	// 	return
+	// }
+	// timer, err := strconv.ParseFloat(arg[2], 32)
+	// if err != nil {
+	// 	fmt.Println("Error while parsing `" + arg[2] + "`")
+	// 	os.Exit(0)
+	//}
 	data, err := os.ReadFile("examples/simple")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 
 	}
+	fmt.Println(string(data))
 
 	production := production.Production{}
 	production.Timeout = true
